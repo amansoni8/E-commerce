60882ac54c4e55bc85b3045d8e21dfafa957719c
aman <amansoni22103@gmail.com>
2024-08-06 18:04:19 +0530

    established connection with db

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import morgan from "morgan";
+import connectDB from "./config/db.js";
 //config env
 dotenv.config(); 
 
+//database config 
+connectDB();
 //rest object
 const app  = express()
+
+//middlewares
+app.use(express.json())
+app.use(morgan('dev'))
 
 app.get('/',(req,res) => {
     res.send({
@@ -13,7 +20,7 @@ app.get('/',(req,res) => {
     })
 })
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080;
 
 
 app.listen(PORT, () =>{
